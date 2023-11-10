6bda27ca92d747dd31bc2f4bf0a51ebb463f49c7
Osamu <1226924006@qq.com>
2023-11-10 19:44:08 +0800

    自定义配置

--- vuecli-demo/vue.config.js
+++ vuecli-demo/vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+// vue.config.js vue脚手架项目默认的配置文件名
+// webpack + node 环境 导出配置对象
+module.exports = {
+  devServer: {
+    port: 3000,
+    open: true   // 浏览器自动打开
+  }
+}
