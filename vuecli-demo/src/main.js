e7c76a7e3d716ca19f2b2b2bb8ccd8179e088340
Osamu <1226924006@qq.com>
2023-11-10 19:30:46 +0800

    创建脚手架项目

--- vuecli-demo/src/main.js
+++ vuecli-demo/src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue'   // 引入vue对象 类似于<script src="vue.js"></script>
+import App from './App.vue'   // 将App.vue文件里对象引入过来 （vue项目页面入口）
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false   // 在控制台有一句提示消息
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+new Vue({   // 实例化vue
+  render: h => h(App),   // render函数：准备渲染App页面
+}).$mount('#app')   // 渲染到index.html文件里 id叫app的标签上
