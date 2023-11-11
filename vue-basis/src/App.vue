16f0cf8e48019234ac90fe9513820875caae1cd9
Osamu <1226924006@qq.com>
2023-11-11 17:54:30 +0800

    插值表达式

---
+++ vue-basis/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- 将值赋给标签 -->
+    <h1>{{ msg }}</h1>
+    <h2>{{ obj.name }}</h2>
+    <h3>{{ obj.age >= 18 ? "成年了" : "未成年" }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  // 变量在data函数return的对象上
+  data() {
+    return {
+      msg: "Hello, vue",
+      obj: {
+        name: "小v",
+        age: 5,
+      },
+    };
+  },
+};
+</script>
+
+<style></style>
