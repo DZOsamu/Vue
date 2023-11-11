e4eee1c58137d86d8718769191825b11c6d9d16a
Osamu <1226924006@qq.com>
2023-11-11 16:45:12 +0800

    清空脚手架自带页面

--- vuecli-demo/src/main.js
+++ vuecli-demo/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'   // 将App.vue文件里对象引入过来 （vue项
 
 Vue.config.productionTip = false   // 在控制台有一句提示消息
 
-let a = 10
+// let a = 10
 
 new Vue({   // 实例化vue
   render: h => h(App),   // render函数：准备渲染App页面
