d68145ec9f6f2ff9c28a8e9dc94378c8048bd7b1
Osamu <1226924006@qq.com>
2023-11-11 20:39:00 +0800

    v-bind指令

--- vue-basis/src/App.vue
+++ vue-basis/src/App.vue
@@ -4,6 +4,10 @@
     <h1>{{ msg }}</h1>
     <h2>{{ obj.name }}</h2>
     <h3>{{ obj.age >= 18 ? "成年了" : "未成年" }}</h3>
+
+    <!-- v-bind指令 -->
+    <a v-bind:href="url">点击</a>
+    <img :src="imgUrl" />
   </div>
 </template>
 
@@ -17,6 +21,9 @@ export default {
         name: "小v",
         age: 5,
       },
+      url: "https://www.miyoushe.com/ys/",
+      imgUrl:
+        "https://uploadstatic.mihoyo.com/ys-obc/2021/12/24/79434666/c7bcb0952b0c6b6d7483dce9f6a3e9c1_2846214008406406765.png",
     };
   },
 };
