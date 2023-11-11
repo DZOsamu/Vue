75a9a0f2a099c258573053381680398214598e8d
Osamu <1226924006@qq.com>
2023-11-11 16:11:53 +0800

    暂时关闭eslint检查

--- vuecli-demo/src/main.js
+++ vuecli-demo/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'   // 将App.vue文件里对象引入过来 （vue项
 
 Vue.config.productionTip = false   // 在控制台有一句提示消息
 
+let a = 10
+
 new Vue({   // 实例化vue
   render: h => h(App),   // render函数：准备渲染App页面
 }).$mount('#app')   // 渲染到index.html文件里 id叫app的标签上
--- vuecli-demo/vue.config.js
+++ vuecli-demo/vue.config.js
@@ -9,5 +9,6 @@ module.exports = {
   devServer: {
     port: 3000,
     open: true   // 浏览器自动打开
-  }
+  },
+  lintOnSave: false   // 暂时关闭eslint检查
 }
