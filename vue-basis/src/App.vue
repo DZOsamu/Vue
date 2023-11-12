92cdf4e0d994cccfc397393988aee34c41ce560d
Osamu <1226924006@qq.com>
2023-11-12 12:28:55 +0800

    v-on指令

--- vue-basis/src/App.vue
+++ vue-basis/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <!-- 将值赋给标签 -->
-    <!-- <h1>{{ msg }}</h1>
+    <h1>{{ msg }}</h1>
     <h2>{{ obj.name }}</h2>
-    <h3>{{ obj.age >= 18 ? "成年了" : "未成年" }}</h3> -->
+    <h3>{{ obj.age >= 18 ? "成年了" : "未成年" }}</h3>
 
     <!-- v-bind指令 -->
-    <!-- <a v-bind:href="url">点击</a>
-    <img :src="imgUrl" /> -->
+    <a v-bind:href="url">点击</a>
+    <img :src="imgUrl" />
 
     <!-- v-on指令 -->
     <p>你要购买的商品数量：{{ count }}</p>
@@ -23,13 +23,13 @@ export default {
   // 变量在data函数return的对象上
   data() {
     return {
-      // msg: "Hello, vue",
-      // obj: {
-      //   name: "小v",
-      //   age: 5,
-      // },
-      // url: "https://www.miyoushe.com/ys/",
-      // imgUrl:"https://uploadstatic.mihoyo.com/ys-obc/2021/12/24/79434666/c7bcb0952b0c6b6d7483dce9f6a3e9c1_2846214008406406765.png",
+      msg: "Hello, vue",
+      obj: {
+        name: "小v",
+        age: 5,
+      },
+      url: "https://www.miyoushe.com/ys/",
+      imgUrl:"https://uploadstatic.mihoyo.com/ys-obc/2021/12/24/79434666/c7bcb0952b0c6b6d7483dce9f6a3e9c1_2846214008406406765.png",
       count: 1,
     };
   },
