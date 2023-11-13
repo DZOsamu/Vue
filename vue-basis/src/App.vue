95ef0b7170884c64d4ae7de9a46e8a38df12f10e
Osamu <1226924006@qq.com>
2023-11-13 21:25:31 +0800

    v-model

--- vue-basis/src/App.vue
+++ vue-basis/src/App.vue
@@ -33,6 +33,59 @@
       <input type="text" @keydown.enter="enterFn" />
       <input type="text" @keydown.esc="escFn" />
     </div>
+
+    <!-- v-model -->
+    <span>用户名：</span>
+    <input type="text" v-model="username" />
+    <span>密码：</span>
+    <input type="password" v-model="password" />
+    <div>
+      <span>来自于：</span>
+      <!-- 下拉菜单要绑定在select上 -->
+      <select v-model="from">
+        <option value="北京市">北京</option>
+        <option value="南京市">南京</option>
+        <option value="天津市">天津</option>
+      </select>
+
+      <!-- 复选框要注意！！！ -->
+      <!-- v-model的vue变量 如果是非数组的形式 关联的是复选框的checked属性 -->
+      <!-- v-model的vue变量 如果是数组的形式 关联的是复选框的value属性 -->
+      <div>
+        <span>角色：</span>
+        <input type="checkbox" value="枫原万叶" v-model="hobby" />枫原万叶
+        <input type="checkbox" value="魈" v-model="hobby" />魈
+        <input type="checkbox" value="流浪者" v-model="hobby" />流浪者
+      </div>
+
+      <div>
+        <span>性别：</span>
+        <input type="radio" name="sex" value="男" v-model="gender" />男
+        <input type="radio" name="sex" value="女" v-model="gender" />女
+      </div>
+
+      <div>
+        <span>自我介绍</span>
+        <textarea v-model="intro"></textarea>
+      </div>
+
+      <!-- v-model修饰符 -->
+      <div>
+        <span>年龄：</span>
+        <input type="number" v-model.number="age" />
+      </div>
+
+      <div>
+        <span>人生格言：</span>
+        <input type="text" v-model.trim="motto" />
+      </div>
+
+      <div>
+        <span>个人简介：</span>
+        <textarea v-model.lazy="introo"></textarea>
+        <!-- 失去焦点时，把内容同步给v-model变量 -->
+      </div>
+    </div>
   </div>
 </template>
 
@@ -50,6 +103,15 @@ export default {
       imgUrl:
         "https://uploadstatic.mihoyo.com/ys-obc/2021/12/24/79434666/c7bcb0952b0c6b6d7483dce9f6a3e9c1_2846214008406406765.png",
       count: 1,
+      username: "123654",
+      password: "",
+      from: "北京市",
+      hobby: [],
+      gender: "男",
+      intro: "",
+      age: 0,
+      motto: "",
+      introo: "",
     };
   },
   methods: {
