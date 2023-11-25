20b1503ae5bd69404eb97468d97e780467a7eaad
Osamu <1226924006@qq.com>
2023-11-25 21:04:46 +0800

    v-for

--- v-for/vue.config.js
+++ v-for/vue.config.js
@@ -2,3 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  devServer: {
+    open: true   // 浏览器自动打开
+  },
+}
