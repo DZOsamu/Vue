3713a1184112806eee8e4e736cbed5171bc6c35c
Osamu <1226924006@qq.com>
2023-11-29 17:26:22 +0800

    v-for更新检测

--- v-for/src/App.vue
+++ v-for/src/App.vue
@@ -4,7 +4,9 @@
     <!-- 让谁循环生成 v-for就写在谁身上 -->
     <p>学生姓名</p>
     <ul>
-      <li v-for="(index, item) in arr" :key="item">{{ index }} --- {{ item }}</li>
+      <li v-for="(index, item) in arr" :key="item">
+        {{ index }} --- {{ item }}
+      </li>
     </ul>
 
     <p>学生详细信息</p>
@@ -18,10 +20,22 @@
 
     <!-- v-for遍历对象（了解） -->
     <p>老师信息</p>
-    <div v-for="(value, key) in tobj" :key="value">{{ value }} === {{ key }}</div>
+    <div v-for="(value, key) in tobj" :key="value">
+      {{ value }} === {{ key }}
+    </div>
     <!-- v-for遍历整数（了解） 从1开始遍历 -->
     <p>序号</p>
     <div v-for="i in count" :key="i">{{ i }}</div>
+
+    <!-- v-for更新检测 -->
+    <div>
+      <ul>
+        <li v-for="item in array" :key="item">{{ item }}</li>
+      </ul>
+      <button @click="revFn">翻转</button>
+      <button @click="sliFn">截取前三个</button>
+      <button @click="modFn">修改第一个</button>
+    </div>
   </div>
 </template>
 
@@ -50,7 +64,24 @@ export default {
         class: "1期",
       },
       count: 10,
+      array: [5, 1, 9, 3, 7],
     };
+  },
+  methods: {
+    revFn() {
+      // v-for更新
+      this.array.reverse();
+    },
+    sliFn() {
+      // slice方法返回新数组 v-for不更新
+      let newArray = this.array.slice(0, 3);
+      this.array = newArray; // 用新数组覆盖实现
+    },
+    modFn() {
+      // 更新某个值 v-for检测不到 不更新
+      // this.array[0] = 100;
+      this.$set(this.array, 0, 100);   // (修改对象,修改位置,修改内容)
+    },
   },
 };
 </script>
