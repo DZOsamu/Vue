837acc855b85e4ec7c7b2abbce0e4499f10bb4cc
Osamu <1226924006@qq.com>
2024-03-09 19:28:06 +0800

    动态样式

---
+++ vue-style/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p :class="{ redStr: bool }">动态class</p>
+    <p :style="{ background: colorStr }">动态style</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bool: true,
+      colorStr: "red",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.redStr {
+  color: red;
+}
+</style>
