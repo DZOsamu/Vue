5e3fd59447b054ae4b683eee53a13674daf270c9
Osamu <1226924006@qq.com>
2024-03-12 20:26:38 +0800

    过滤器

--- case-brandmanagement/src/main.js
+++ case-brandmanagement/src/main.js
@@ -5,6 +5,10 @@ import "bootstrap/dist/css/bootstrap.css"
 
 Vue.config.productionTip = false
 
+// 方式1：全局过滤器（在任意的.vue文件内可以直接使用）
+// 语法：Vue.filter("过滤器名",值 => 处理后结果)
+Vue.filter("reverse", val => val.split("").reverse().join(""))
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
