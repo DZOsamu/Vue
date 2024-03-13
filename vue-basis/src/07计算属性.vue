1d5ac49e78d2d30a340902d11f7161f5e78562f6
Osamu <1226924006@qq.com>
2024-03-13 20:40:33 +0800

    计算属性

---
+++ vue-basis/src/07计算属性.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>{{ num }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      a: 10,
+      b: 20,
+    };
+  },
+
+  // 计算属性：
+  // 场景：一个变量的值，需要用另外的变量计算得到
+  // 注意：计算属性也是vue变量，所以不能和data里的重名，用法和data相同
+  /*语法：
+    computed: {
+      计算属性名() {
+        return 值
+      }
+    }
+  */
+  computed: {
+    num() {
+      return this.a + this.b;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
