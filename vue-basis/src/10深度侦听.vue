ecc510b4358b9752f24234b0fce611491a00ea84
Osamu <1226924006@qq.com>
2024-04-15 20:43:22 +0800

    深度侦听

---
+++ vue-basis/src/10深度侦听.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input type="text" v-model="user.name" />
+    <input type="text" v-model="user.age" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: "",
+        age: 0,
+      },
+    };
+  },
+  // 侦听对象
+  watch: {
+    user: {
+      handler(newVal, oldVal) {
+        console.log(newVal, oldVal);
+      },
+      deep: true, // 深度侦听
+      immediate: true, // 立即侦听（网页打开handler执行一次）
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- vue-basis/src/App.vue
+++ vue-basis/src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <div>
-    <input type="text" v-model="name" />
-  </div>
+  
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      name: "",
-    };
-  },
-  // 侦听name值的改变
-  watch: {
-    name(newVal, oldVal) {
-      // newVal 当前最新值
-      // oldVal 上一刻的值
-      console.log(newVal, oldVal);
-    },
-  },
-};
+
+}
 </script>
 
 <style>
+
 </style>
